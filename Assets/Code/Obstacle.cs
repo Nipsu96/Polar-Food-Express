cdf1f981d21b09bc559c6b558af7f941581ee786
Cats <tomi.sepanmaa@gmail.com>
2022-03-29 08:36:49 +0300

    WIP: ObjectPooling

--- Assets/Code/Obstacle.cs
+++ Assets/Code/Obstacle.cs
@@ -11,5 +11,11 @@ namespace Polar
             // Calls GameManager to end the game
             GameManager.Instance.EndGame();
         }
-    }
+
+		public void OnDespawn()
+		{
+			// Set this obstacle inactive if it collides with a despawner.
+			gameObject.SetActive(false);
+		}
+	}
 }
