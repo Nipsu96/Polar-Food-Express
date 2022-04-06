23f4e58954648bb0ed30943b47907e0432dc48b8
Cats <tomi.sepanmaa@gmail.com>
2022-04-06 14:25:28 +0300

    WIP: Trying to fix build

--- Assets/Code/ButtonManager.cs
+++ Assets/Code/ButtonManager.cs
@@ -9,13 +9,20 @@ namespace Polar
     {
         private string mainMenuName = "MainMenu";
         private string kauppahalliName = "Kauppahalli";
-        private GameObject mainMenu;
+		private string optionsName = "OptionsMenu";
+		private string scoreboardName = "ScoreboardMenu";
+		private GameObject mainMenu;
         private GameObject optionsMenu;
         private GameObject scoreboardMenu;
 
-        private void Start()
+		private void Awake()
+		{
+			Debug.LogWarning("Awake");
+		}
+
+		private void Start()
         {
-			print("Game Started");
+			Debug.LogWarning("Start");
             FindMainMenu();
             FindOptionsMenu();
             FindScoreboardMenu();
@@ -79,7 +86,7 @@ namespace Polar
         {
             if (mainMenu == null)
             {
-                mainMenu = transform.Find("MainMenu").gameObject;
+                mainMenu = transform.Find(mainMenuName).gameObject;
             }
         }
 
@@ -87,7 +94,7 @@ namespace Polar
         {
             if (optionsMenu == null)
             {
-                optionsMenu = transform.Find("OptionsMenu").gameObject;
+                optionsMenu = transform.Find(optionsName).gameObject;
             }
         }
 
@@ -95,7 +102,7 @@ namespace Polar
         {
             if (scoreboardMenu == null)
             {
-                scoreboardMenu = transform.Find("ScoreboardMenu").gameObject;
+                scoreboardMenu = transform.Find(scoreboardName).gameObject;
             }
         }
     }
