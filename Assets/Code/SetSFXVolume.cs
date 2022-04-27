0ae6c1f154bd81c410c17ad52e381bef1a1c59ea
Cats <tomi.sepanmaa@gmail.com>
2022-04-27 19:06:53 +0300

    Slider art. Saving audio levels on PlayerPrefs

--- Assets/Code/SetSFXVolume.cs
+++ Assets/Code/SetSFXVolume.cs
@@ -12,7 +12,7 @@ namespace Polar
         public void SetLevel(float sliderValue)
         {
             mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
-
-        }
+			PlayerPrefs.SetFloat("SFXVolume", sliderValue);
+		}
     }
 }
